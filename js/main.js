05fdf43c156e3845fb0ece90b93fe2b856ee69ef
Alberto <albertomutcirilo@gmail.com>
2020-05-13 01:00:58 +0200

    wip

--- js/main.js
+++ js/main.js
@@ -1,13 +1,42 @@
 function call() {
+  var data;
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4) {
-      var data = xhr.responseText;
-      alert(data);
+      data = JSON.parse(xhr.responseText);
     }
   };
   xhr.open('POST', 'http://localhost:8081/cryptonite/cryptoprices', true);
   xhr.send(
-    "{'service':'cryptopriceservice', 'start':'1','limit':'3','convert':'EUR'}"
+    "{'service':'cryptopriceservice', 'start':'1','limit':'4','convert':'EUR'}"
   );
+
+  xhr.onload = function () {
+    if (this.status == 200) {
+      var output = '';
+
+      for (var i in data) {
+        output +=
+          '<ul class="list-inline">' +
+          '<li><img src="images/' +
+          data[i].symbol +
+          '.png" /></li>' +
+          '<li id="coin-name">' +
+          data[i].name +
+          '</li>' +
+          '<li id="coin-symbol">' +
+          data[i].symbol +
+          '</li>' +
+          '<li id="coin-price">' +
+          data[i].price +
+          '</li>' +
+          '<li id="coin-percent-change">' +
+          data[i].percent_change +
+          '</li>' +
+          '</ul>';
+      }
+      console.log(data);
+      document.getElementById('coins').innerHTML = output;
+    }
+  };
 }
